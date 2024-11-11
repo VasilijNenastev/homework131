4b7c486862213f6ee1c4920edd7b937509f1f8fa
1 <vasilijnenastev@gmail.com>
2024-11-11 19:15:26 +0400

    HW13

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
+    public static String checkingTheYea(int year) {
+        if (year<1854){return " год не может быть меньше 1854";}
 
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return year + " - високосный год";
+        } else {
+            return year + " - не високосный год";
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(checkingTheYea(1800));
     }
 }
